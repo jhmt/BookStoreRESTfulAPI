cc1604cabd21fd33c55d7c166fda40cdef2290e6
jhmt <jhashimot00@gmail.com>
2016-02-09 23:00:24 +1300

    Update MapsConfig

--- TheBookStore/TheBookStore/App_Start/MapsConfig.cs
+++ TheBookStore/TheBookStore/App_Start/MapsConfig.cs
@@ -10,35 +10,37 @@ namespace TheBookStore
 {
     public static class MapsConfig
     {
+        private static MapperConfiguration _config;
+        private static IMapper _mapper;
+
         public static void Register()
         {
-            Mapper.CreateMap<Book, BookDto>()
-                  .ForMember(b => b.Authors, m => m.MapFrom(a => a.Authors));
-
-            Mapper.CreateMap<Author, BookAuthorsDto>()
-                  .ForMember(b => b.FullName, m => m.MapFrom(a => a.Name + " " + a.Surname))
-                  .ForSourceMember(b => b.Books, m => m.Ignore());
-
-            Mapper.CreateMap<Book, AuthorBooksDto>();
-                  
-            Mapper.CreateMap<Author, AuthorDto>()
-                  .ForMember(b => b.FullName, m => m.MapFrom(a => a.Name + " " + a.Surname))
-                  .ForMember(a => a.Books, m => m.MapFrom(b => b.Books))
-                  .ForSourceMember(b => b.Books, m => m.Ignore());
-                        
-            Mapper.CreateMap<Review, ReviewDto>()
-                  .ForMember(r => r.BookId, m => m.MapFrom(s => s.BookId));
+            _config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Book, BookDto>()
+                   .ForMember(b => b.Authors, m => m.MapFrom(a => a.Authors));
+                cfg.CreateMap<Author, BookAuthorsDto>()
+                   .ForMember(b => b.FullName, m => m.MapFrom(a => a.Name + " " + a.Surname))
+                   .ForSourceMember(b => b.Books, m => m.Ignore());
+                cfg.CreateMap<Book, AuthorBooksDto>();
+                cfg.CreateMap<Author, AuthorDto>()
+                   .ForMember(b => b.FullName, m => m.MapFrom(a => a.Name + " " + a.Surname))
+                   .ForMember(a => a.Books, m => m.MapFrom(b => b.Books))
+                   .ForSourceMember(b => b.Books, m => m.Ignore());
+                cfg.CreateMap<Review, ReviewDto>()
+                    .ForMember(r => r.BookId, m => m.MapFrom(s => s.BookId));
+            });
+            _mapper = _config.CreateMapper();
         }
 
         public static T To<T>(this object source)
         {
-            
-            return Mapper.Map<T>(source);
+            return _mapper.Map<T>(source);
         }
 
         public static IEnumerable<T> To<T>(this IEnumerable<object> source)
         {
-            return Mapper.Map<IEnumerable<T>>(source);
+            return _mapper.Map<IEnumerable<T>>(source);
         }
     }
 }
