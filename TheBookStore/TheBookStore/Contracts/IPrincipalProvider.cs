f94e045e81f5169cdc8953f9cc301dc8bc7fb932
jhmt <jhashimot00@gmail.com>
2016-02-14 20:29:22 +1300

    Add Custom Basic Authentication

---
+++ TheBookStore/TheBookStore/Contracts/IPrincipalProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBookStore.Contracts
+{
+    public interface IPrincipalProvider
+    {
+        IPrincipal CreatePrincipal(string username, string password);
+    }
+}
